61fa08849926201fd2f518eeee6604a151ceb315
ADS\lrrocha <lrrocha@iu.edu>
2019-02-18 09:49:02 -0500

    Added Count Text and Win Text variables. Game now counts pickups and displays win text when all are collected

--- RollABall/Assets/Scripts/Player.cs
+++ RollABall/Assets/Scripts/Player.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public float speed = 1;
+    public Text countText;
+    public Text winText;
+    private int count;
+
+    private void Start()
+    {
+        count = 0;
+        SetCountText ();
+        winText.text = "";
+    }
 
     // move the player based on input
     private void FixedUpdate()
@@ -17,11 +28,21 @@ public class Player : MonoBehaviour
         GetComponent<Rigidbody>().AddForce(move*speed);
     }
 
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText ();
+
         }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+            winText.text = "You Win!";
     }
 }
